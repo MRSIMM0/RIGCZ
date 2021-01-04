7d7f0a473c159d60b4d5f6631e180827529436c6
MRSIMM0 <s.pasieczny02@gmail.com>
2021-01-04 17:45:25 +0100

    X

--- rigcz_counter/src/main/java/pl/simmo/rigcz_counter/RoleProvider.java
+++ rigcz_counter/src/main/java/pl/simmo/rigcz_counter/RoleProvider.java
@@ -1,24 +1,24 @@
-package pl.simmo.rigcz_counter;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import pl.simmo.rigcz_counter.Entity.Role;
-import pl.simmo.rigcz_counter.Entity.RoleName;
-import pl.simmo.rigcz_counter.Repository.RoleRepository;
-
-@Component
-public class RoleProvider {
-
-//    private RoleRepository roleRepository;
+//package pl.simmo.rigcz_counter;
 //
-//    public RoleProvider(RoleRepository roleRepository) {
-//        this.roleRepository = roleRepository;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//import pl.simmo.rigcz_counter.Entity.Role;
+//import pl.simmo.rigcz_counter.Entity.RoleName;
+//import pl.simmo.rigcz_counter.Repository.RoleRepository;
+//
+//@Component
+//public class RoleProvider {
+//
+////    private RoleRepository roleRepository;
+////
+////    public RoleProvider(RoleRepository roleRepository) {
+////        this.roleRepository = roleRepository;
+////    }
+////
+////    @Override
+////    public void run(String... args) throws Exception {
+////       roleRepository.save(new Role(RoleName.ROLE_USER));
+////       roleRepository.save(new Role(RoleName.ROLE_ADMIN));
+////       roleRepository.save(new Role(RoleName.ROLE_PM));
 //    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//       roleRepository.save(new Role(RoleName.ROLE_USER));
-//       roleRepository.save(new Role(RoleName.ROLE_ADMIN));
-//       roleRepository.save(new Role(RoleName.ROLE_PM));
-    }
-}
+//}
