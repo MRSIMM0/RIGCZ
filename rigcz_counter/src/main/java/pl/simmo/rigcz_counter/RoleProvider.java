b6842884bffab5d6998799500bfc498f979cedc3
MRSIMM0 <s.pasieczny02@gmail.com>
2021-01-04 17:44:07 +0100

    X

--- rigcz_counter/src/main/java/pl/simmo/rigcz_counter/RoleProvider.java
+++ rigcz_counter/src/main/java/pl/simmo/rigcz_counter/RoleProvider.java
@@ -7,18 +7,18 @@ import pl.simmo.rigcz_counter.Entity.RoleName;
 import pl.simmo.rigcz_counter.Repository.RoleRepository;
 
 @Component
-public class RoleProvider implements CommandLineRunner {
+public class RoleProvider {
 
-    private RoleRepository roleRepository;
-
-    public RoleProvider(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-       roleRepository.save(new Role(RoleName.ROLE_USER));
-       roleRepository.save(new Role(RoleName.ROLE_ADMIN));
-       roleRepository.save(new Role(RoleName.ROLE_PM));
+//    private RoleRepository roleRepository;
+//
+//    public RoleProvider(RoleRepository roleRepository) {
+//        this.roleRepository = roleRepository;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//       roleRepository.save(new Role(RoleName.ROLE_USER));
+//       roleRepository.save(new Role(RoleName.ROLE_ADMIN));
+//       roleRepository.save(new Role(RoleName.ROLE_PM));
     }
 }
