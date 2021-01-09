449b785b733e593befa8f7e726186edd763c5c2d
MRSIMM0 <s.pasieczny02@gmail.com>
2021-01-09 19:35:10 +0100

    deleting entity

--- rigcz_counter/src/main/java/pl/simmo/rigcz_counter/Contreoller/EntityController.java
+++ rigcz_counter/src/main/java/pl/simmo/rigcz_counter/Contreoller/EntityController.java
@@ -33,13 +33,14 @@ public class EntityController {
         this.service = service;
         this.userRepo = userRepo;
     }
-//    @PreAuthorize("hasRole('ADMIN') or hasRole('PM') or hasRole('USER')")
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PM') or hasRole('USER')")
     @PostMapping("/new")
     public DbEntity add(@RequestBody DbEntityMod entity, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new Error("error");
         }
-        DbEntity entity1 = new DbEntity(entity.getName(), 0, userRepo.findUserByUsername(entity.getCraetedBy()).orElseThrow(()-> new RuntimeException("Unknown Creator")), null, null);
+        DbEntity entity1 = new DbEntity(entity.getName(), 0, userRepo.findUserByUsername(entity.getCraetedBy()).orElseThrow(() -> new RuntimeException("Unknown Creator")), null, null);
         return entityRepo.save(entity1);
     }
 
@@ -57,25 +58,26 @@ public class EntityController {
         Set<User> userMinus = db.getUsersMinus();
 
 
-            User user = userRepo.findUserByUsername(dbEntity.getUpdatingUser())
-                    .orElseThrow(() -> new RuntimeException("User doesn't exist"));
+        User user = userRepo.findUserByUsername(dbEntity.getUpdatingUser())
+                .orElseThrow(() -> new RuntimeException("User doesn't exist"));
 
-            if (usersAdd.contains(user)) {
-                return ResponseEntity.badRequest().body("User already used this option.");
-            }
-            if (userMinus.contains(user)) {
-                userMinus.remove(user);
-                db.setRigczLevel(db.getRigczLevel() + 1);
-            }
-            usersAdd.add(user);
+        if (usersAdd.contains(user)) {
+            return ResponseEntity.badRequest().body("User already used this option.");
+        }
+        if (userMinus.contains(user)) {
+            userMinus.remove(user);
             db.setRigczLevel(db.getRigczLevel() + 1);
-            db.setUsersAdd(usersAdd);
+        }
+        usersAdd.add(user);
+        db.setRigczLevel(db.getRigczLevel() + 1);
+        db.setUsersAdd(usersAdd);
 
 
         entityRepo.save(db);
 
         return ResponseEntity.ok().build();
     }
+
     @PreAuthorize("hasRole('ADMIN') or hasRole('PM') or hasRole('USER')")
     @PostMapping("/minus")
     public ResponseEntity<String> miuns(@RequestBody DbEntityMod dbEntity) {
@@ -85,21 +87,21 @@ public class EntityController {
         Set<User> userMinus = db.getUsersMinus();
 
 
-            User user = userRepo.findUserByUsername(dbEntity.getUpdatingUser())
-                    .orElseThrow(() -> new RuntimeException("User doesn't exist"));
+        User user = userRepo.findUserByUsername(dbEntity.getUpdatingUser())
+                .orElseThrow(() -> new RuntimeException("User doesn't exist"));
 
-            if (userMinus.contains(user)) {
-                return ResponseEntity.badRequest().body("User already used this option.");
-            }
+        if (userMinus.contains(user)) {
+            return ResponseEntity.badRequest().body("User already used this option.");
+        }
 
-            if (usersAdd.contains(user)) {
-                usersAdd.remove(user);
-                db.setRigczLevel(db.getRigczLevel() - 1);
-            }
-
-            userMinus.add(user);
+        if (usersAdd.contains(user)) {
+            usersAdd.remove(user);
             db.setRigczLevel(db.getRigczLevel() - 1);
-            db.setUsersMinus(userMinus);
+        }
+
+        userMinus.add(user);
+        db.setRigczLevel(db.getRigczLevel() - 1);
+        db.setUsersMinus(userMinus);
 
 
         entityRepo.save(db);
@@ -108,24 +110,49 @@ public class EntityController {
     }
 
     @GetMapping("/getUserEntities/{username}")
-            @PreAuthorize("hasRole('ADMIN') or hasRole('PM') or hasRole('USER')")
-    ResponseEntity<?> getUserEntities(@PathVariable String username){
-       List<DbEntity> dbEntity = entityRepo.findByCreatedBy(userRepo.findUserByUsername(username).orElseThrow(() -> new RuntimeException("Unknown User.")));
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PM') or hasRole('USER')")
+    ResponseEntity<?> getUserEntities(@PathVariable String username) {
+        List<DbEntity> dbEntity = entityRepo.findByCreatedBy(userRepo.findUserByUsername(username).orElseThrow(() -> new RuntimeException("Unknown User.")));
 
-       List<DbEntityMod> entity = dbEntity.stream().map(e ->{
+        List<DbEntityMod> entity = dbEntity.stream().map(e -> {
 
-           DbEntityMod mod =  new DbEntityMod();
+            DbEntityMod mod = new DbEntityMod();
 
-           mod.setId(e.getId());
-           mod.setName(e.getName());
-           mod.setRigczLevel(e.getRigczLevel());
-           mod.setCraetedBy(e.getCreatedBy().getUsername());
-           mod.setUserAdd(e.getUsersAdd().stream().map(user -> user.getUsername()).collect(Collectors.toSet()));
-           mod.setUserMinus(e.getUsersAdd().stream().map(user -> user.getUsername()).collect(Collectors.toSet()));
+            mod.setId(e.getId());
+            mod.setName(e.getName());
+            mod.setRigczLevel(e.getRigczLevel());
+            mod.setCraetedBy(e.getCreatedBy().getUsername());
+            mod.setUserAdd(e.getUsersAdd().stream().map(user -> user.getUsername()).collect(Collectors.toSet()));
+            mod.setUserMinus(e.getUsersAdd().stream().map(user -> user.getUsername()).collect(Collectors.toSet()));
 
-           return mod;
-       }).collect(Collectors.toList());
+            return mod;
+        }).collect(Collectors.toList());
 
-      return ResponseEntity.ok(entity);
+        return ResponseEntity.ok(entity);
+    }
+
+    @PostMapping("/deleteEntity")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PM') or hasRole('USER')")
+    public ResponseEntity<String> deleteEntity(@RequestBody DbEntityMod entity) {
+        if (entityRepo.existsById(entity.getId())) {
+            User requester = userRepo.findUserByUsername(entity.getUpdatingUser()).orElseThrow(() -> new RuntimeException("user doesn't exist"));
+            requester.getRoles().forEach(role -> {
+                switch (role.getName()) {
+                    case ROLE_PM:
+                    case ROLE_ADMIN:
+                        entityRepo.deleteById(entity.getId());
+                        break;
+                    case ROLE_USER:
+                        User creator = userRepo.findUserByUsername(entity.getCraetedBy()).orElseThrow(() -> new RuntimeException("Creator doesn't exist"));
+                        if (creator != requester) {
+                            throw new RuntimeException("You can't do that");
+                        }
+                        entityRepo.deleteById(entity.getId());
+                        break;
+                }
+            });
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
